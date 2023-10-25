8282f25fc8438b440fade355439fcab94e26b61e
Anomali99 <fatiqnur@gmail.com>
2023-10-25 20:23:40 +0700

    first commit

---
+++ src/latihan/Mahasiswa.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan;
+
+/**
+ *
+ * @author fatiq
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String alamat;
+    private String no_tlp;
+
+    /**
+     * @return the nim
+     */
+    public String getNim() {
+        return nim;
+    }
+
+    /**
+     * @param nim the nim to set
+     */
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the no_tlp
+     */
+    public String getNo_tlp() {
+        return no_tlp;
+    }
+
+    /**
+     * @param no_tlp the no_tlp to set
+     */
+    public void setNo_tlp(String no_tlp) {
+        this.no_tlp = no_tlp;
+    }
+}
